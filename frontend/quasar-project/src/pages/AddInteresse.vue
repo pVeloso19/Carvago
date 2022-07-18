00a0286823147e86d14cc3385bff575f7482b500
Pedro Veloso <miguelveloso.25@hotmail.com>
2022-07-18 16:10:58 +0100

    +

--- frontend/quasar-project/src/pages/AddInteresse.vue
+++ frontend/quasar-project/src/pages/AddInteresse.vue
@@ -1,20 +1,13 @@
 <template>
     <p>Adicionar Interesse</p>
-    <Teste v-on:updateTeste="teste = $event" />
-    <p>{{teste}}</p>
 </template>
 
 <script>
 
 import { ref } from 'vue'
-import Teste from 'components/teste.vue'
 
 export default {
   name: 'AddInteressePage',
-
-  components: {
-    Teste
-  },
 
   setup () {
     const teste = ref('')
